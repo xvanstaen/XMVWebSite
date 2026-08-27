d89657ae0ff274e73f708ea10c235e6d63030e54
xvanstaen <xvanstaen@gmail.com>
2026-08-27 13:54:27 +0800

    E

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { Buffer } from 'buffer';
-
-(window as any).Buffer = Buffer;
-(window as any).global = window;
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
