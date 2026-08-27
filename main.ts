3072430765070777ec1fbb94fe845800f45d0d8e
xvanstaen <xvanstaen@gmail.com>
2026-08-27 15:02:25 +0800

    I

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { appConfig } from './src/app/app.config';
 import { AppComponent } from './src/app/app.component';
 import { Buffer } from 'buffer';
+import { environment } from './src/environments/environment';
 
+if (environment.production) {
+  enableProdMode();
+}
 (window as any).Buffer = Buffer;
 (window as any).global = window;
 bootstrapApplication(AppComponent, appConfig)
